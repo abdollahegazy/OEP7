980c6851e590051967f1fae86d1a19758c6d5ca5
Abdolla Hegazy <abdolla.hegazy2006@gmail.com>
2025-10-23 18:15:11 -0400

    adding sync script

---
+++ sync_to_icer.sh
@@ -0,0 +1,6 @@
+
+    #  --exclude='OEP7' \
+    #  --exclude='__pycache__' \
+    #  --exclude='initial_sasa' \
+    #  --exclude='build' \
+   rsync -auvrP  ./simulations ./toppar h200:/mnt/scratch/hegazyab/oep7/
